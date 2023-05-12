35b92f906c7e3c5d18207ce4fa359e321aa5d6e2
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2023-05-12 15:22:00 -0400

    Update types.jl

--- src/types/types.jl
+++ src/types/types.jl
@@ -1,6 +1,28 @@
-Base.@kwdef struct F8widths
-    sgnbits::Int8=I8(1)
-    expbits::Int8=I8(5)
-    sigbits::Int8=I8(2)
+struct F8bits
+    sgnbits::Int8
+    expbits::Int8
+    sigbits::Int8
 end
+
+function F8bits(sgnbits, expbits, sigbits)
+    nbits = sgnbits + expbits + sigbits
+    nbits != 8 && throw(DomainError("\"F8 must use exactly 8 bits\""))
+    F8bits(Int8(sgnbits), Int8(expbits), Int8(sigbits))
+end
+
+struct F8masks
+    sgnmask::UInt8
+    expmask::UInt8
+    sigmask::UInt8
+end
+   
+function F8masks(f8::F8bits)
+    sgnmask = 0x80
+    expmask = ((0x01 << f8.expbits) - 0x01) << f8.sigbits
+    sigmask = (0x01 << f8.sigbits) - 0x01
+    F8masks(sgnmask, expmask, sigmask)
+end
+
+
+
 

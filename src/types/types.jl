41be83322f201d4ddfc25f2dd4447e3aad46c9eb
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2023-05-12 15:36:10 -0400

    Update types.jl

--- src/types/types.jl
+++ src/types/types.jl
@@ -23,25 +23,21 @@ function F8masks(f8::F8bits)
     F8masks(sgnmask, expmask, sigmask)
 end
 
+struct F8expon
+    bias::Int8
+    emin::Int8 # least exponent        (signed)
+    emax::Int8 # greatest exponent    (signed)
+    mine::Int8 # smallest raw exponent (skips reserved exponent value[s])
+    maxe::Int8 # largest raw exponent  (skips reserved exponent value[s])
+end
+ 
+function F8expon(bias::Int, emin::Int, emax::Int, mine::Int, maxe::Int)
+    F8expon(I8(bias), I8(emin), I8(emax), I8(mine), I8(maxe))
+end
+ 
 struct F8values
     bits::F8bits
     masks::F8masks
-    bias::Int8
-    emin::Int8
-    emax::Int8
-    smin::Rational{Int8}
-    smax::Rational{Int8}    
+    expon::F8expon
 end
     
-    
-    
-struct F8parts
-    sgn::Int8
-    xpn::Int8  # raw exponent
-    exp::Int8  # biased exponent
-    sig::Int8  # significand / 2^k (implicitly)
-    sgb::Int8  # 1 + sig
-end
-
-
-

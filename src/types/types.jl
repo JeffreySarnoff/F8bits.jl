6982eaba5501657acee650a5a440f60ec296d4ba
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2023-05-13 03:27:44 -0400

    Update types.jl

--- src/types/types.jl
+++ src/types/types.jl
@@ -41,7 +41,7 @@ characteristics of an 8-bit float's exponent field
 struct F8expon
     bias::Int8
     emin::Int8 # least exponent        (signed)
-    emax::Int8 # greatest exponent    (signed)
+    emax::Int8 # greatest exponent     (signed)
     mine::Int8 # smallest raw exponent (skips reserved exponent value[s])
     maxe::Int8 # largest raw exponent  (skips reserved exponent value[s])
 end
@@ -49,7 +49,23 @@ end
 function F8expon(bias::Int, emin::Int, emax::Int, mine::Int, maxe::Int)
     F8expon(I8(bias), I8(emin), I8(emax), I8(mine), I8(maxe))
 end
- 
+
+function F8expon(ebits, emin, emax)
+    bias = -emin
+    mine = 0
+    maxe = emax + bias
+
+    F8expon(bias, emin, emax, mine, maxe)
+end
+
+function F8expon(ebits)
+    bias = (2^ebits - 1) - 1
+    emin = -bias
+    emax = (2^ebits - 1) - bias
+
+    F8expon(ebits, emin, emax)
+end
+
 """
      F8sortal
 

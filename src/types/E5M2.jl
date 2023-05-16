62972227ebb1c32641c11eb6fbe3ab84a44b2199
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2023-05-15 22:08:51 -0400

    Update E5M2.jl

--- src/types/E5M2.jl
+++ src/types/E5M2.jl
@@ -3,6 +3,23 @@ const Emax =  15
 const Emin = -14
 const Binades = 30
 
+const SgnBits = 1
+const ExpBits = 5
+const SigBits = 2
+const SgnMask = 0b1_00000_00
+const ExpMask = 0b0_11111_00
+const SigMask = 0b0_00000_11
+const SgnShift = 7
+const ExpShift = 2
+const SigShift = 0
+
+const Specials = 8
+isspecial(x::UInt8) = (x & ExpMask) === ExpMask
+isnormal(x::UInt8)  = 0b0_00001_00 <= (x & ExpMask) <= 0b0_11110_00
+issubnormal(x::UInt8) = iszero(x & ExpMask) && !iszero(x & SigMask)
+isinf(x::UInt8) =  (x & ~SgnMask) === 0b0_11111_10
+signbit(x::UInt8) = !iszero(x & SgnMask)
+
 const NaN52           = 0b1_11111_11
 const PosNaN          = 0b0_11111_11
 const PosInf52        = 0b0_11111_10
@@ -25,6 +42,7 @@ const MidNegSubnormal = 0b1_00000_10 # -(1/2) * (1/2^14)
 const MinPosSubnormal = 0b0_00000_01 #  1/2^16
 const MinNegSubnormal = 0b1_00000_01 # -1/2^16
 
+#=
 # 30 groups of 4
 0b0_00001_00 == 0x04       # (1 + 0/4) * 2^-14 (2^(1-Bias))
 0b0_00001_01               # (1 + 1/4) * 2^-14
@@ -36,4 +54,6 @@ const MinNegSubnormal = 0b1_00000_01 # -1/2^16
 0b0_11110_01               # (1+1/4) * 2^15 
 0b0_11110_10               # (1+1/2) * 2^15
 0b0_11110_11 == 0x7b (123) # (1+3/4) * 2^15
+=#
+
 

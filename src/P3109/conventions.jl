1d4763aae26f7737443f9c3a2919eb5fb58d2369
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2023-05-22 22:03:54 -0400

    Update conventions.jl

--- src/P3109/conventions.jl
+++ src/P3109/conventions.jl
@@ -1,10 +1,10 @@
 export NaN8, Zero8, Inf8, 
-       isinfnan, ispositive, isnonnegative, isnonpositive, isnegative
+       isinfnan, ispositive, isnonnegative, isnonpositive, isnegative, isnormal
 
 import Base: reinterpret, 
              exponent_bits, significand_bits,
              sign_mask, exponent_mask, significand_mask,
-             exponent_bias,
+             exponent_bias, exponent_one, exponent_half,
              UInt8, Int8,
              zero, one, iszero, isone,
              isnan, isinf, isreal, isfinite, isnormal, issubnormal,
@@ -28,8 +28,8 @@ Int8(x::Float8{E}) where {E} = reinterpret(Int8, value(x))
 reinterpret(::Type{Unsigned},  ::Type{Float8{E}}) where {E} = UInt8
 reinterpret(::Type{Signed},    ::Type{Float8{E}}) where {E} = Int8
 
-reinterpret(::Type{Unsigned},  x::Float8{E}) where {E} = reinterpret(UInt8, x)
-reinterpret(::Type{Signed},    x::Float8{E}) where {E} = reinterpret(Int8, x)
+reinterpret(::Type{Unsigned},  x::Float8{E}) where {E} = value(x)
+reinterpret(::Type{Signed},    x::Float8{E}) where {E} = reinterpret(Int8, value(x))
 reinterpret(::Type{Float8{E}}, x::UInt8)     where {E} = Float8{E}(x)
 reinterpret(::Type{Float8{E}}, x::Int8)      where {E} = Float8{E}(reinterpret(UInt8, x))
 
@@ -49,9 +49,9 @@ sign_unmask_low(::Type{Float8{E}}) where {E} = ~sign_mask_low(Float8{E})
 exponent_unmask_low(::Type{Float8{E}}) where {E} = ~exponent_mask_low(Float8{E})
 significand_unmask_low(::Type{Float8{E}}) where {E}  = ~significand_mask_low(Float8{E})
 
-sign_mask(::Type{Float8{E}}) where {E} = sign_mask_low(Float8{E}) << sign_shift
-significand_mask(::Type{Float8{E}}) where {E}  = significand_mask_low(Float8{E}) << significand_shift
-exponent_mask(::Type{Float8{E}}) where {E} = exponent_mask_low(Float8{E}) << exponent_shift
+sign_mask(::Type{Float8{E}}) where {E} = sign_mask_low(Float8{E}) << sign_shift(Float8{E})
+significand_mask(::Type{Float8{E}}) where {E}  = significand_mask_low(Float8{E}) << significand_shift(Float8{E})
+exponent_mask(::Type{Float8{E}}) where {E} = exponent_mask_low(Float8{E}) << exponent_shift(Float8{E})
 
 sign_unmask(::Type{Float8{E}}) where {E} = ~sign_mask(Float8{E})
 exponent_unmask(::Type{Float8{E}}) where {E} = ~exponent_mask(Float8{E})
@@ -97,19 +97,22 @@ shift_sign(::Type{Float8{E}}, x::UInt8) where {E} = (x & sign_mask_low(Float8{E}
 shift_exponent(::Type{Float8{E}}, x::UInt8) where {E} = (x & exponent_mask_low(Float8{E})) << exponent_shift(Float8{E})
 shift_significand(::Type{Float8{E}}, x::UInt8) where {E} = (x & significand_mask_low(Float8{E})) << significand_shift(Float8{E})
 
-# Base.exponent_one(::Type{T}) where {T<:AbstractFloat} = exponent_mask(T) - sign_mask(T) >> 1
-# Base.exponent_half(::Type{T}) where {T<:AbstractFloat} = (exponent_one(T) >> significand_bits(T) - 0x01) << significand_bits(T)
-
-iszero(x::Float8{E}) where {E} = (value(x) === 0x00)
-signbit(x::Float8{E}) where {E} = !iszero(value(x) & sign_mask(Float8{E}))
-isnonnegative(x::Float8{E}) where {E} = iszero(x & Base.sign_mask(Float8{E}))
-ispositive(x::Float8{E}) where {E} = iszero(x & Base.sign_mask(Float8{E})) && !iszero(x)
+exponent_one(::Type{T})  where {E, T<:Float8{E}} = exponent_mask(T) - sign_mask(T) >> 1
+exponent_half(::Type{T}) where {E, T<:Float8{E}} = (exponent_one(T) >> significand_bits(T) - 0x01) << significand_bits(T)
 
 const NaN8    = 0b1111_1111 # 0xff
 const Zero8   = 0b0000_0000 # 0x00
 const Inf8    = 0b0111_1110 # 0x7e
 const PosInf8 = 0b0111_1110 # 0x7e
 const NegInf8 = 0b1111_1110 # 0xfe
+
+for I in (0, 1, 2, 3, 4, 5, 6, 7)
+    E = Symbol(:E,I)
+    for Const8 in (:NaN8, :Zero8, :Inf8, :PosInf8, :NegInf8)
+        C8E = Symbol(Const8, E)
+        @eval $C8E = Float8{$I}($Const8)
+    end; 
+end;
 
 iszero(x::Float8{E}) = value(x) === Zero8
 isnan(x::Float8{E}) = value(x) === NaN8
@@ -125,9 +128,13 @@ isnonnegative(x::Float8{E}) where {E} = !signbit(x) && !isnan(x)
 ispositive(x::Float8{E}) where {E} = (!signbit(x) && !iszero(x)) && !isnan(x)
 isnonpositive(x::Float8{E}) where {E} = (signbit(x) || iszero(x)) && !isnan(x)
 
+function issubnormal(x::T) where {E, T<:Float8{E}}
+    y = reinterpret(Unsigned, x)
+    (iszero(y & exponent_mask(T)) & !iszero(y & significand_mask(T))
+end
+       
+isnormal(x::Float8{E}) where {E} = !issubnormal(x) && !isinfnan(x)
 
-Base.issubnormal
-i
 Base.isone
 Base.nextfloat
 Base.prevfloat

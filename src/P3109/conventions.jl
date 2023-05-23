cdc851b53bab97265c0942089a96e4e72d3134a0
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2023-05-22 23:15:45 -0400

    Update conventions.jl

--- src/P3109/conventions.jl
+++ src/P3109/conventions.jl
@@ -29,78 +29,78 @@ Float8{E}(x::Int8) = Float8{E}(reinterpret(UInt8, x))
 
 value(float8::Float8{E}) = float8.x
 
-UInt8(x::Float8{E}) where {E} = value(x)
-Int8(x::Float8{E}) where {E} = reinterpret(Int8, value(x))
+UInt8(x::T) where {E, T<:Float8{E}} = value(x)
+Int8(x::T) where {E, T<:Float8{E}} = reinterpret(Int8, value(x))
   
 # following float.jl loosely
 
-reinterpret(::Type{Unsigned},  ::Type{Float8{E}}) where {E} = UInt8
-reinterpret(::Type{Signed},    ::Type{Float8{E}}) where {E} = Int8
+reinterpret(::Type{Unsigned},  ::Type{T}) where {E, T<:Float8{E}} = UInt8
+reinterpret(::Type{Signed},    ::Type{T}) where {E, T<:Float8{E}} = Int8
 
-reinterpret(::Type{Unsigned},  x::Float8{E}) where {E} = value(x)
-reinterpret(::Type{Signed},    x::Float8{E}) where {E} = reinterpret(Int8, value(x))
+reinterpret(::Type{Unsigned},  x::T) where {E, T<:Float8{E}} = value(x)
+reinterpret(::Type{Signed},    x::T) where {E, T<:Float8{E}} = reinterpret(Int8, value(x))
 reinterpret(::Type{Float8{E}}, x::UInt8)     where {E} = Float8{E}(x)
 reinterpret(::Type{Float8{E}}, x::Int8)      where {E} = Float8{E}(reinterpret(UInt8, x))
 
-sign_bits(::Type{Float8{E}}) where {E} = 0x01
-exponent_bits(::Type{Float8{E}}) where {E} = UInt8(E)
-significand_bits(::Type{Float8{E}}) where {E} = 0x07 - UInt8(E)
+sign_bits(::Type{T}) where {E, T<:Float8{E}} = 0x01
+exponent_bits(::Type{T}) where {E, T<:Float8{E}} = UInt8(E)
+significand_bits(::Type{T}) where {E, T<:Float8{E}} = 0x07 - UInt8(E)
 
-sign_shift(::Type{Float8{E}}) where {E} = 0x07
-exponent_shift(::Type{Float8{E}}) where {E} = significand_bits(Float8{E})
-signficand_shift(::Type{Float8{E}}) where {E} = 0x00
+sign_shift(::Type{T}) where {E, T<:Float8{E}} = 0x07
+exponent_shift(::Type{T}) where {E, T<:Float8{E}} = significand_bits(Float8{E})
+signficand_shift(::Type{T}) where {E, T<:Float8{E}} = 0x00
 
-sign_mask_low(::Type{Float8{E}}) where {E} = 0x01
-exponent_mask_low(::Type{Float8{E}}) where {E} = (0x01 << Base.exponent_bits(Float8{E})) - 0x01
-significand_mask_low(::Type{Float8{E}}) where {E}  = (0x01 << Base.significand_bits(Float8{E})) - 0x01
+sign_mask_low(::Type{T}) where {E, T<:Float8{E}} = 0x01
+exponent_mask_low(::Type{T}) where {E, T<:Float8{E}} = (0x01 << Base.exponent_bits(Float8{E})) - 0x01
+significand_mask_low(::Type{T}) where {E, T<:Float8{E}}  = (0x01 << Base.significand_bits(Float8{E})) - 0x01
 
-sign_unmask_low(::Type{Float8{E}}) where {E} = ~sign_mask_low(Float8{E})
-exponent_unmask_low(::Type{Float8{E}}) where {E} = ~exponent_mask_low(Float8{E})
-significand_unmask_low(::Type{Float8{E}}) where {E}  = ~significand_mask_low(Float8{E})
+sign_unmask_low(::Type{T}) where {E, T<:Float8{E}} = ~sign_mask_low(Float8{E})
+exponent_unmask_low(::Type{T}) where {E, T<:Float8{E}} = ~exponent_mask_low(Float8{E})
+significand_unmask_low(::Type{T}) where {E, T<:Float8{E}}  = ~significand_mask_low(Float8{E})
 
-sign_mask(::Type{Float8{E}}) where {E} = sign_mask_low(Float8{E}) << sign_shift(Float8{E})
-significand_mask(::Type{Float8{E}}) where {E}  = significand_mask_low(Float8{E}) << significand_shift(Float8{E})
-exponent_mask(::Type{Float8{E}}) where {E} = exponent_mask_low(Float8{E}) << exponent_shift(Float8{E})
+sign_mask(::Type{T}) where {E, T<:Float8{E}} = sign_mask_low(Float8{E}) << sign_shift(Float8{E})
+significand_mask(::Type{T}) where {E, T<:Float8{E}}  = significand_mask_low(Float8{E}) << significand_shift(Float8{E})
+exponent_mask(::Type{T}) where {E, T<:Float8{E}} = exponent_mask_low(Float8{E}) << exponent_shift(Float8{E})
 
-sign_unmask(::Type{Float8{E}}) where {E} = ~sign_mask(Float8{E})
-exponent_unmask(::Type{Float8{E}}) where {E} = ~exponent_mask(Float8{E})
-significand_unmask(::Type{Float8{E}}) where {E}  = ~significand_mask(Float8{E})
+sign_unmask(::Type{T}) where {E, T<:Float8{E}} = ~sign_mask(Float8{E})
+exponent_unmask(::Type{T}) where {E, T<:Float8{E}} = ~exponent_mask(Float8{E})
+significand_unmask(::Type{T}) where {E, T<:Float8{E}}  = ~significand_mask(Float8{E})
 
-sign_bits(x::Float8{E}) where {E} = sign_bits(Float8{E})
-exponent_bits(x::Float8{E}) where {E} = exponent_bits(Float8{E})
-significand_bits(x::Float8{E}) where {E} = significand_bits(Float8{E})
+sign_bits(x::T) where {E, T<:Float8{E}} = sign_bits(Float8{E})
+exponent_bits(x::T) where {E, T<:Float8{E}} = exponent_bits(Float8{E})
+significand_bits(x::T) where {E, T<:Float8{E}} = significand_bits(Float8{E})
 
-sign_shift(x::Float8{E}) where {E} = sign_shift(Float8{E})
-exponent_shift(x::Float8{E}) where {E} = exponent_shift(Float8{E})
-significand_shift(x::Float8{E}) where {E} = significand_shift(Float8{E})
+sign_shift(x::T) where {E, T<:Float8{E}} = sign_shift(Float8{E})
+exponent_shift(x::T) where {E, T<:Float8{E}} = exponent_shift(Float8{E})
+significand_shift(x::T) where {E, T<:Float8{E}} = significand_shift(Float8{E})
 
-sign_mask_low(x::Float8{E}) where {E} = sign_mask_low(Float8{E}) & value(x)
-exponent_mask_low(x::Float8{E}) where {E} = exponent_mask_low(Float8{E}) & value(x)
-significand_mask_low(x::Float8{E}) where {E} = significand_mask_low(Float8{E}) & value(x)
+sign_mask_low(x::T) where {E, T<:Float8{E}} = sign_mask_low(Float8{E}) & value(x)
+exponent_mask_low(x::T) where {E, T<:Float8{E}} = exponent_mask_low(Float8{E}) & value(x)
+significand_mask_low(x::T) where {E, T<:Float8{E}} = significand_mask_low(Float8{E}) & value(x)
 
-sign_unmask_low(x::Float8{E}) where {E} = sign_unmask_low(Float8{E}) & value(x)
-exponent_unmask_low(x::Float8{E}) where {E} = exponent_unmask_low(Float8{E}) & value(x)
-significand_unmask_low(x::Float8{E}) where {E} = significand_unmask_low(Float8{E}) & value(x)
+sign_unmask_low(x::T) where {E, T<:Float8{E}} = sign_unmask_low(Float8{E}) & value(x)
+exponent_unmask_low(x::T) where {E, T<:Float8{E}} = exponent_unmask_low(Float8{E}) & value(x)
+significand_unmask_low(x::T) where {E, T<:Float8{E}} = significand_unmask_low(Float8{E}) & value(x)
 
-sign_mask(x::Float8{E}) where {E} = sign_mask(Float8{E}) & value(x)
-exponent_mask(x::Float8{E}) where {E} = exponent_mask(Float8{E}) & value(x)
-significand_mask(x::Float8{E}) where {E} = significand_mask(Float8{E}) & value(x)
+sign_mask(x::T) where {E, T<:Float8{E}} = sign_mask(Float8{E}) & value(x)
+exponent_mask(x::T) where {E, T<:Float8{E}} = exponent_mask(Float8{E}) & value(x)
+significand_mask(x::T) where {E, T<:Float8{E}} = significand_mask(Float8{E}) & value(x)
 
-sign_unmask(x::Float8{E}) where {E} = sign_unmask(Float8{E}) & value(x)
-exponent_unmask(x::Float8{E}) where {E} = exponent_unmask(Float8{E}) & value(x)
-significand_unmask(x::Float8{E}) where {E} = significand_unmask(Float8{E}) & value(x)
+sign_unmask(x::T) where {E, T<:Float8{E}} = sign_unmask(Float8{E}) & value(x)
+exponent_unmask(x::T) where {E, T<:Float8{E}} = exponent_unmask(Float8{E}) & value(x)
+significand_unmask(x::T) where {E, T<:Float8{E}} = significand_unmask(Float8{E}) & value(x)
 
-unshift_sign(x::Float8{E}) where {E} = (value(x) & sign_mask(Float8{E})) >> sign_shift(Float8{E})
-unshift_exponent(x::Float8{E}) where {E} = (value(x) & exponent_mask(Float8{E})) >> exponent_shift(Float8{E})
-unshift_significand(x::Float8{E}) where {E} = (value(x) & significand_mask(Float8{E})) >> signficand_shift(Float8{E})
+unshift_sign(x::T) where {E, T<:Float8{E}} = (value(x) & sign_mask(Float8{E})) >> sign_shift(Float8{E})
+unshift_exponent(x::T) where {E, T<:Float8{E}} = (value(x) & exponent_mask(Float8{E})) >> exponent_shift(Float8{E})
+unshift_significand(x::T) where {E, T<:Float8{E}} = (value(x) & significand_mask(Float8{E})) >> signficand_shift(Float8{E})
 
 unshift_sign(::Type{Float8{E}}, x::UInt8) where {E} = (x & sign_mask(Float8{E})) >> sign_shift(Float8{E})
 unshift_exponent(::Type{Float8{E}}, x::UInt8) where {E} = (x & exponent_mask(Float8{E})) >> exponent_shift(Float8{E})
 unshift_significand(::Type{Float8{E}}, x::UInt8) where {E} = (x & significand_mask(Float8{E})) >> significand_shift(Float8{E})
 
-shift_sign(x::Float8{E}) where {E} = (value(x) & sign_mask_low(Float8{E})) << sign_shift(Float8{E})
-shift_exponent(x::Float8{E}) where {E} = (value(x) & exponent_mask_low(Float8{E})) << exponent_shift(Float8{E})
-shift_significand(x::Float8{E}) where {E} = (value(x) & significand_mask_low(Float8{E})) << signficand_shift(Float8{E})
+shift_sign(x::T) where {E, T<:Float8{E}} = (value(x) & sign_mask_low(Float8{E})) << sign_shift(Float8{E})
+shift_exponent(x::T) where {E, T<:Float8{E}} = (value(x) & exponent_mask_low(Float8{E})) << exponent_shift(Float8{E})
+shift_significand(x::T) where {E, T<:Float8{E}} = (value(x) & significand_mask_low(Float8{E})) << signficand_shift(Float8{E})
 
 shift_sign(::Type{Float8{E}}, x::UInt8) where {E} = (x & sign_mask_low(Float8{E})) << sign_shift(Float8{E})
 shift_exponent(::Type{Float8{E}}, x::UInt8) where {E} = (x & exponent_mask_low(Float8{E})) << exponent_shift(Float8{E})
@@ -123,28 +123,28 @@ for I in (0, 1, 2, 3, 4, 5, 6, 7)
     end; 
 end;
 
-iszero(x::Float8{E}) where {E} = value(x) === Zero8
-isnan(x::Float8{E}) where {E} = value(x) === NaN8
-isinf(x::Float8{E}) where {E} = (value(x) & sign_unmask(Float8{E})) === Inf8
-isposinf(x::Float8{E}) where {E} = value(x) === Inf8
-isneginf(x::Float8{E}) where {E} = value(x) === NegInf8
-isreal(x::Float8{E}) where {E} = !isnan(x)
-isfinite(x::Float8{E}) where {E} = !isinf(x) && !isnan(x)
-isinfnan(x::Float8{E}) where {E} = isinf(x) || isnan(x)
+iszero(x::T) where {E, T<:Float8{E}} = value(x) === Zero8
+isnan(x::T) where {E, T<:Float8{E}} = value(x) === NaN8
+isinf(x::T) where {E, T<:Float8{E}} = (value(x) & sign_unmask(Float8{E})) === Inf8
+isposinf(x::T) where {E, T<:Float8{E}} = value(x) === Inf8
+isneginf(x::T) where {E, T<:Float8{E}} = value(x) === NegInf8
+isreal(x::T) where {E, T<:Float8{E}} = !isnan(x)
+isfinite(x::T) where {E, T<:Float8{E}} = !isinf(x) && !isnan(x)
+isinfnan(x::T) where {E, T<:Float8{E}} = isinf(x) || isnan(x)
 
-signbit(x::Float8{E}) where {E} = (value(x) & sign_mask(Float8{E})) === sign_mask(Float8{E})
+signbit(x::T) where {E, T<:Float8{E}} = (value(x) & sign_mask(Float8{E})) === sign_mask(Float8{E})
 
-isnegative(x::Float8{E}) where {E} = signbit(x) && !isnan(x)
-isnonnegative(x::Float8{E}) where {E} = !signbit(x) && !isnan(x)
-ispositive(x::Float8{E}) where {E} = (!signbit(x) && !iszero(x)) && !isnan(x)
-isnonpositive(x::Float8{E}) where {E} = (signbit(x) || iszero(x)) && !isnan(x)
+isnegative(x::T) where {E, T<:Float8{E}} = signbit(x) && !isnan(x)
+isnonnegative(x::T) where {E, T<:Float8{E}} = !signbit(x) && !isnan(x)
+ispositive(x::T) where {E, T<:Float8{E}} = (!signbit(x) && !iszero(x)) && !isnan(x)
+isnonpositive(x::T) where {E, T<:Float8{E}} = (signbit(x) || iszero(x)) && !isnan(x)
 
 function issubnormal(float8::T) where {E, T<:Float8{E}}
     x = value(float8)
     (iszero(x & exponent_mask(T)) & !iszero(x & significand_mask(T))
 end
 
-isnormal(x::Float8{E}) where {E} = !issubnormal(x) && !isinfnan(x)
+isnormal(x::T) where {E, T<:Float8{E}} = !issubnormal(x) && !isinfnan(x)
 
 function isinteger(x::T) where {E, T<:Float8{E}}
    ax = abs(x)

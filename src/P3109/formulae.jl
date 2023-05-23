7210eb390dde6ad23387633ffa68f0851bc621d8
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2023-05-23 06:58:55 -0400

    Update formulae.jl

--- src/P3109/formulae.jl
+++ src/P3109/formulae.jl
@@ -9,3 +9,17 @@ unbias_754(exp_bits, raw_exp) = raw_exp - bias_754(exp_bits)
 # floating point exponent scaled
 scale_sym(exp_bits, raw_exp) = (2.0f0)^unbias_sym(exp_bits, raw_exp)
 scale_754(exp_bits, raw_exp) = (2.0f0)^unbias_754(exp_bits, raw_exp)
+
+# floating point significand without and with implicit bit
+explicit_sig(sig_bits, raw_sig) = raw_sig / 2.0f0^sig_bits
+implicit_sig(sig_bits, raw_sig) = 1.0f0 + explicit_sig(sig_bits, raw_sig)
+
+# positive normal floating point values
+pos_normal_754(exp_bits, sig_bits, raw_exp, raw_sig) = implicit_sig(sig_bits, raw_sig) * scale_754(exp_bits, raw_exp)
+pos_normal_sym(exp_bits, sig_bits, raw_exp, raw_sig) = implicit_sig(sig_bits, raw_sig) * scale_sym(exp_bits, raw_exp)
+
+# positive subnormal floating point values
+pos_subnormal_754(exp_bits, sig_bits, raw_exp, raw_sig) = explicit_sig(sig_bits, raw_sig) * scale_754(exp_bits, raw_exp)
+pos_sibnormal_sym(exp_bits, sig_bits, raw_exp, raw_sig) = explicit_sig(sig_bits, raw_sig) * scale_sym(exp_bits, raw_exp)
+
+

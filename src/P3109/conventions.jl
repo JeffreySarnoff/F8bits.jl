f32205023c202d1c4127561ab988514ad811eeca
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2023-05-23 06:02:13 -0400

    Update conventions.jl

--- src/P3109/conventions.jl
+++ src/P3109/conventions.jl
@@ -38,14 +38,13 @@ Float8{E}(x::Int16) where {E} = !signbit(x) ? Float8{E}(reinterpret(UInt16, x)) 
 Float8{E}(x::Int32) where {E} = !signbit(x) ? Float8{E}(reinterpret(UInt32, x)) : Float8{E}((reinterpret(UInt32, -x) % UInt8) | sign_mask(Float8{E})
 Float8{E}(x::Int64) where {E} = !signbit(x) ? Float8{E}(reinterpret(UInt64, x)) : Float8{E}((reinterpret(UInt64, -x) % UInt8) | sign_mask(Float8{E})
 
-UInt8(x::T) where {E, T<:Float8{E}} = value(x)
-Int8(x::T) where {E, T<:Float8{E}} = reinterpret(Int8, value(x))
-       
+Base.UInt8(x::T) where {E, T<:Float8{E}} = value(x)
+Base.Int8(x::T) where {E, T<:Float8{E}} = reinterpret(Int8, value(x))
 
-exponent_bits(::Type{T}) where {E, T<:Float8{E}} = UInt8(E)
-significand_bits(::Type{T}) where {E, T<:Float8{E}} = 0x07 - UInt8(E)
-exponent_bits(float8::T) where {E, T<:Float8{E}} = exponent_bits(T)
-significand_bits(float8::T) where {E, T<:Float8{E}} = significand_bits(T)
+Base.exponent_bits(::Type{T}) where {E, T<:Float8{E}} = UInt8(E)
+Base.significand_bits(::Type{T}) where {E, T<:Float8{E}} = 0x07 - UInt8(E)
+Base.exponent_bits(x::T) where {E, T<:Float8{E}} = exponent_bits(T)
+Base.significand_bits(x::T) where {E, T<:Float8{E}} = significand_bits(T)
 
 
   

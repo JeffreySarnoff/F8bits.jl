d136ac0cc3440c0bb1b4ec5b71514f9487d63221
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2023-05-23 07:04:09 -0400

    Update formulae.jl

--- src/P3109/formulae.jl
+++ src/P3109/formulae.jl
@@ -3,8 +3,8 @@ bias_sym(exp_bits) = Int8( 2^(exp_bits - 1) )
 bias_754(exp_bits) = Int8( bias_sym(exp_bits) - 1 )
 
 # floating point exponent unbiased
-unbias_sym(exp_bits, raw_exp) = raw_exp - bias_sym(exp_bits)
-unbias_754(exp_bits, raw_exp) = raw_exp - bias_754(exp_bits)
+unbias_sym(exp_bits, raw_exp) = Int8(raw_exp) - bias_sym(exp_bits)
+unbias_754(exp_bits, raw_exp) = Int8(raw_exp) - bias_754(exp_bits)
 
 # floating point exponent scaled
 scale_sym(exp_bits, raw_exp) = (2.0f0)^unbias_sym(exp_bits, raw_exp)
